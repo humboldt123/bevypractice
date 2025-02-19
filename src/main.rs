1295d339d6cf8c2f388894f5e745733efd20dd40
Vish <38266782+humboldt123@users.noreply.github.com>
2025-02-19 14:20:39 -0500

    feat: window

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,45 @@
 use bevy::prelude::*;
+use bevy::{
+    core::FrameCount,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    prelude::*,
+    window::{CursorGrabMode, PresentMode, SystemCursorIcon, WindowLevel, WindowTheme},
+    winit::cursor::CursorIcon,
+};
+
+const BACKGROUND: Color = Color::srgb(0.298, 0.298, 0.298);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .insert_resource(ClearColor(BACKGROUND))
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Platformer Demo".into(),
+                    name: Some("bevy.app".into()),
+                    resolution: (800., 600.).into(),
+                    present_mode: PresentMode::AutoVsync,
+                    fit_canvas_to_parent: true,
+                    prevent_default_event_handling: false,
+                    window_theme: Some(WindowTheme::Dark),
+                    enabled_buttons: bevy::window::EnabledButtons {
+                        maximize: false,
+                        ..Default::default()
+                    },
+                    ..default()
+                }),
+                ..default()
+            }),
+            LogDiagnosticsPlugin::default(),
+            FrameTimeDiagnosticsPlugin,
+        ))
         .add_systems(Startup, setup)
         .add_systems(Update, sprite_movement)
         .run();
 }
+
+
+
 
 #[derive(Component)]
 enum Direction {
